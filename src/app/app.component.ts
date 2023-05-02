88a4f62a3e46f30351993d277970ce50a646e34d
samgabriel7624 <samgabrielrj2000@gmail.com>
2023-05-02 10:53:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+ 
+import { ProductService } from './product.service';
+import { Product } from './product.service';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dependency';
+ 
+export class AppComponent
+{
+   products:Product[] | undefined;
+   productService;
+ 
+   constructor(){
+     this.productService=new ProductService();
+   }
+ 
+   getProducts() {
+     
+     this.products=this.productService.getProducts();
+   }
+ 
 }
